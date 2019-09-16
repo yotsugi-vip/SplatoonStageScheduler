10c70ee87655b7684372ae22d681b0c4c1c75087
yotugi <harukipdr@gmail.com>
2019-09-16 23:43:17 +0900

    htmlとcss調整 jsonは取得方法がわかったので表示が確立するまでローカルファイルから取得

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.on('window-all-closed', function() {
 // Electronの初期化完了後に実行
 app.on('ready', function() {
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({maxWidth: 800, maxHeight: 600});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // ウィンドウが閉じられたらアプリも終了
